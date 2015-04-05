c2c103af308432fe4a9c9b9809a4eb1f984c1858
Maximilian Irro <max@irro.at>
2015-04-05 18:16:45 +0200

    added "webm" to the recognised file extensions

--- lib/vollbremsung.rb
+++ lib/vollbremsung.rb
@@ -1,10 +1,10 @@
 module Vollbremsung
   
   USAGE   = "Usage: vollbremsung [options] target [target [...]]"
-  VERSION = '0.0.19'
-  CONVERT_TYPES = ['mkv','avi','mov','flv','mpg','wmv','ogm']
+  VERSION = '0.0.19'.freeze
+  CONVERT_TYPES = ['avi','flv','mkv','mpg','mov','ogm','webm','wmv']
   FFMPEG_OPTIONS = "-map 0 -acodec copy -vcodec copy -scodec copy"
-  X264_DEFAULT_PRESET = "veryfast"
+  X264_DEFAULT_PRESET = "veryfast".freeze
   
   class StreamDescr < Struct.new(:count,:names)
     def initialize
