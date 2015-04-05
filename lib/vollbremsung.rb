aebdc509e58071afc574649503216d8159f7b1ca
Maximilian Irro <max@irro.at>
2015-04-05 18:17:57 +0200

    v0.0.20

--- lib/vollbremsung.rb
+++ lib/vollbremsung.rb
@@ -1,7 +1,7 @@
 module Vollbremsung
   
   USAGE   = "Usage: vollbremsung [options] target [target [...]]"
-  VERSION = '0.0.19'.freeze
+  VERSION = '0.0.20'.freeze
   CONVERT_TYPES = ['avi','flv','mkv','mpg','mov','ogm','webm','wmv']
   FFMPEG_OPTIONS = "-map 0 -acodec copy -vcodec copy -scodec copy"
   X264_DEFAULT_PRESET = "veryfast".freeze
